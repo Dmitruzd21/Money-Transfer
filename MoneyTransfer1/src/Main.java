e4c4d4baa48e051ca66296d6370942a0829e89d7
Korotkov Dmitry <2110821@mail.ru>
2021-07-13 18:15:16 +0300

    Добавлен проект с кодом ПО

---
+++ MoneyTransfer1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentaccountbalance = 2_000_000_000;
+        int transferamount = 500_000_000;
+        int finalaccountbalance = currentaccountbalance + transferamount;
+        System.out.println(finalaccountbalance);
+    }
+}
